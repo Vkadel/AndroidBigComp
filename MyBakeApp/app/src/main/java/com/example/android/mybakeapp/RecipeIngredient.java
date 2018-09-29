80cc560c9d50a08ecee0f9c022708579ab6b6911
Vkadel <35178154+Vkadel@users.noreply.github.com>
2018-09-28 23:53:06 -0400

    Initial commit

---
+++ MyBakeApp/app/src/main/java/com/example/android/mybakeapp/RecipeIngredient.java
@@ -0,0 +1,47 @@
+package com.example.android.mybakeapp;
+
+public class RecipeIngredient {
+int mIngredientId;
+double mIngredientQuantity;
+String mIngredientMeasure;
+String mIngredientName;
+    public RecipeIngredient(int ingredientId, double ingredientQuantity,
+                            String ingredientMeasure, String ingredientName){
+        mIngredientId=ingredientId;
+        mIngredientQuantity=ingredientQuantity;
+        mIngredientMeasure=ingredientMeasure;
+        mIngredientName=ingredientName;
+    }
+
+    public void setmIngredientId(int mIngredientId) {
+        this.mIngredientId = mIngredientId;
+    }
+
+    public void setmIngredientMeasure(String mIngredientMeasure) {
+        this.mIngredientMeasure = mIngredientMeasure;
+    }
+
+    public void setmIngredientName(String mIngredientName) {
+        this.mIngredientName = mIngredientName;
+    }
+
+    public void setmIngredientQuantity(int mIngredientQuantity) {
+        this.mIngredientQuantity = mIngredientQuantity;
+    }
+
+    public int getmIngredientId() {
+        return mIngredientId;
+    }
+
+    public double getmIngredientQuantity() {
+        return mIngredientQuantity;
+    }
+
+    public String getmIngredientMeasure() {
+        return mIngredientMeasure;
+    }
+
+    public String getmIngredientName() {
+        return mIngredientName;
+    }
+}
