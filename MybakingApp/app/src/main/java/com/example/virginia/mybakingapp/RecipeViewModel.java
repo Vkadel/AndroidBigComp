f4569371419d46e0aca33aa5d09f5f7223489638
Vkadel <35178154+Vkadel@users.noreply.github.com>
2018-09-29 16:38:14 -0400

    Update 4:37pm

--- MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/RecipeViewModel.java
+++ MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/RecipeViewModel.java
@@ -2,20 +2,42 @@ package com.example.virginia.mybakingapp;
 
 import android.arch.lifecycle.ViewModel;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 import com.example.virginia.mybakingapp.Internet.GetString;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import timber.log.Timber;
 
-public class RecipeViewModel extends ViewModel   {
+public class RecipeViewModel extends ViewModel{
+    ArrayList<Recipe> myRecipies;
+
     public RecipeViewModel() {
         super();
-        String myRecipes= String.valueOf(new GetString());
-        Timber.e(myRecipes);
+
+
+    }
+public class myCalltoGetdata extends AsyncTask{
+
+    @Override
+    protected void onPostExecute(Object o) {
+        //Update Data
+        super.onPostExecute(o);
     }
 
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        GetString getString=new GetString();
+        getString;
+        return null;
+    }
+}
 
 
 }
