e62dc09baf8607f0069394517d938788d106a54d
Vkadel <35178154+Vkadel@users.noreply.github.com>
2018-09-29 17:21:42 -0400

    Update 5:21pm

--- MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/Internet/GetString.java
+++ MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/Internet/GetString.java
@@ -11,23 +11,28 @@ import okhttp3.Response;
 
 public class GetString {
     OkHttpClient client = new OkHttpClient();
-
+    String myResponse;
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    String run(String url) throws IOException {
+    public String run(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            myResponse=response.body().string();
+            return myResponse;
         }
     }
         @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-        public static void main(String[] args) throws IOException {
+
+         public static void main(String[] args) throws IOException {
             GetString example = new GetString();
             String response = example.run("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
             System.out.println(response);
 
+    }
+    public String getStringBack(){
+        return myResponse;
     }
 
 }
--- MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/RecipeViewModel.java
+++ MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/RecipeViewModel.java
@@ -3,7 +3,9 @@ package com.example.virginia.mybakingapp;
 import android.arch.lifecycle.ViewModel;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.AsyncTaskLoader;
 
 import com.example.virginia.mybakingapp.Internet.GetString;
 
@@ -21,23 +23,39 @@ public class RecipeViewModel extends ViewModel{
     public RecipeViewModel() {
         super();
 
+        new myCalltoGetdata().execute();
 
     }
-public class myCalltoGetdata extends AsyncTask{
+
+}
+
+class myCalltoGetdata extends AsyncTask<Long, Long, ArrayList<Recipe>> {
+    GetString getString;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected ArrayList<Recipe> doInBackground(Long... longs) {
+        ArrayList<Recipe> myRecipesLil=new ArrayList<>();
+        getString=new GetString();
+        try {
+            getString.run("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myRecipesLil;
+
+    }
 
     @Override
-    protected void onPostExecute(Object o) {
-        //Update Data
-        super.onPostExecute(o);
+    protected void onPostExecute(ArrayList<Recipe> recipes) {
+        super.onPostExecute(recipes);
+        String myResponse=getString.getStringBack();
+        Timber.e(myResponse);
     }
 
-    @Override
-    protected Object doInBackground(Object[] objects) {
-        GetString getString=new GetString();
-        getString;
-        return null;
-    }
+
 }
 
 
-}
+
+
