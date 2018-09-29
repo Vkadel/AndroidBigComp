8b96944cc4f71a3fa360d5ed78ccbfd167af3023
Vkadel <35178154+Vkadel@users.noreply.github.com>
2018-09-29 11:53:54 -0400

    11:53

--- MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/RecipeStep.java
+++ MybakingApp/app/src/main/java/com/example/virginia/mybakingapp/RecipeStep.java
@@ -1,5 +1,13 @@
 package com.example.virginia.mybakingapp;
 
 public class RecipeStep {
-    
+    int stepId;
+    String stepShortDesc;
+    String stepDesc;
+    String stepVidURL;
+    String stepThumburl;
+
+    public RecipeStep(){
+
+    }
 }
